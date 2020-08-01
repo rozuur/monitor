2097a86667f9bf38b5f735b8ce2179c27e15b938
Naveen Vardhi <rozuur@gmail.com>
2020-08-01 18:30:19 +0530

    Initial stub for showing the status of a pidfile

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,37 @@
 extern crate clap;
 
+use clap::{App, Arg};
+
+fn show_status(filename: &str) {
+    println!("Show status of {}", filename)
+}
+
 fn main() {
-    clap::App::new("Monitor programs").name("monitor").version("0.1").get_matches();
+    let matches = App::new("Monitor programs")
+        .name("monitor")
+        .version("0.1")
+        .arg(
+            Arg::with_name("status")
+                .short("S")
+                .long("status")
+                .help("check status of --pidfile"),
+        )
+        .arg(
+            Arg::with_name("pidfile")
+                .short("p")
+                .long("pidfile")
+                .value_name("FILE")
+                .help("write pid to <path>"),
+        )
+        .get_matches();
+
+    if matches.is_present("status") {
+        if !matches.is_present("pidfile") {
+            eprintln!("Error: --pidfile required");
+            std::process::exit(1);
+        }
+        let pidfile = matches.value_of("pidfile").unwrap_or("");
+        show_status(pidfile);
+        std::process::exit(0);
+    }
 }
